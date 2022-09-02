ed32e515f9959641590ed7fdb83183c28cd9340f
yooseek <68885418+yooseek@users.noreply.github.com>
2022-09-02 22:42:40 +0900

    cash - restaurant_detail

--- lib/restaurant/view/restaurant_detail_screen.dart
+++ lib/restaurant/view/restaurant_detail_screen.dart
@@ -5,45 +5,44 @@ import 'package:code_factory2/product/component/product_card.dart';
 import 'package:code_factory2/restaurant/component/restaurant_card.dart';
 import 'package:code_factory2/restaurant/model/restaurant_detail_model.dart';
 import 'package:code_factory2/restaurant/repository/restaurant_repository.dart';
+import 'package:code_factory2/restaurant/riverpod/restaurant_provider.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../model/restaurant_model.dart';
 
 class RestaurantDetailScreen extends ConsumerWidget {
   final String id;
   final String name;
 
-  const RestaurantDetailScreen({required this.id,required this.name, Key? key}) : super(key: key);
+  const RestaurantDetailScreen({required this.id, required this.name, Key? key})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final state = ref.watch(restaurantDetailProvider(id));
+
+    if (state == null) {
+      return Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+
     return DefaultLayout(
       title: name,
-      child: FutureBuilder<RestaurantDetailModel>(
-        future: ref.watch(restaurantRepositoryProvider).getRestaurantDetail(id: id),
-        builder: (context,AsyncSnapshot<RestaurantDetailModel> snapshot) {
-          if(!snapshot.hasData){
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-
-          // 두 개의 리스트가 존재하고 대신 하나의 리스트처럼 움직이고 싶을 때
-          return renderCustomScrollView(item: snapshot.data!);
-        },
-      ),
-
+      child: // 두 개의 리스트가 존재하고 대신 하나의 리스트처럼 움직이고 싶을 때
+          renderCustomScrollView(item: state!),
     );
   }
 
-  CustomScrollView renderCustomScrollView({required RestaurantDetailModel item}) {
-
+  CustomScrollView renderCustomScrollView({required RestaurantModel item}) {
     // 두 개의 리스트가 존재하고 대신 하나의 리스트처럼 움직이고 싶을 때
     return CustomScrollView(
       slivers: [
         // 일반 위젯을 sliver 형태로 넣을 때
         SliverToBoxAdapter(
-          child: RestaurantCard.fromModel(model: item,isDetail: true),
+          child: RestaurantCard.fromModel(model: item, isDetail: true),
         ),
 
         // 일반 위젯을 sliver 형태로 넣을 때 - Label
@@ -62,7 +61,7 @@ class RestaurantDetailScreen extends ConsumerWidget {
           padding: const EdgeInsets.symmetric(horizontal: 16.0),
           sliver: SliverList(
             delegate: SliverChildBuilderDelegate(
-                  (context, index) {
+              (context, index) {
                 return Padding(
                   padding: const EdgeInsets.only(top: 16.0),
                   child: ProductCard.fromModel(model: item.products[index]),
